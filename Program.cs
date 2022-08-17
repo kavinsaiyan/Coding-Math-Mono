2903be12c494c2547e37d9bfa4b0e0c882e3fe9d
kavinsaiyan <37509336+kavinsaiyan@users.noreply.github.com>
2022-08-17 22:47:14 +0530

    episode 2

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace CodingMath
         [STAThread]
         static void Main()
         {
-            using (var game = new Episode1())
+            using (var game = new Episode2())
                 game.Run();
         }
     }
