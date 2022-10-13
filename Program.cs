725c67cb574454cae999d9b8bd9306b6253a5fc2
kavinsaiyan <37509336+kavinsaiyan@users.noreply.github.com>
2022-10-13 23:11:11 +0530

    Mini 8 correction

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace CodingMath
         [STAThread]
         static void Main()
         {
-            using (var game = new Mini7())
+            using (var game = new Mini8())
                 game.Run();
         }
     }
